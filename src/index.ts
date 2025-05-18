e9e7a56942b780e31fbffdaf9937b08b7a912710
YangQF2002 <yangqf18@gmail.com>
2025-05-19 04:05:11 +0800

    Working version

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import { logger } from "hono/logger";
 
 type Bindings = {
   UPLOADTHING_TOKEN: string 
-  ENVIRONMENT: string
+  UPLOADTHING_MODE: string
 }
 
 const app = new Hono<{Bindings: Bindings}>();
-// app.use("*", cors());
-// app.use("*", logger());
+app.use("*", cors());
+app.use("*", logger());
 
 // Request from Expo Frontend 
 app.all("/api/uploadthing", async (c) => {
@@ -24,7 +24,8 @@ app.all("/api/uploadthing", async (c) => {
     config: {
       // Reads the env variables 
       token: c.env.UPLOADTHING_TOKEN,
-      isDev: c.env.ENVIRONMENT === "development",
+      isDev: c.env.UPLOADTHING_MODE === "development",
+
       // Workers doesn't support cache option
       // So we need to remove it from request init
       fetch: (url, init) => {
